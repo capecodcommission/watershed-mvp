d873f110cf7986937940bde4a60a4561f5439874
Sue <sue@bluegear.io>
2016-04-25 11:42:36 -0400

    attempting to add polygon draw tool to testmap

--- resources/views/testmap.blade.php
+++ resources/views/testmap.blade.php
@@ -29,6 +29,15 @@
       "dijit/layout/BorderContainer", 
       "dijit/layout/ContentPane", 
       "dijit/TitlePane",
+      "esri/toolbars/draw",
+		"esri/symbols/SimpleMarkerSymbol", 
+		"esri/symbols/SimpleLineSymbol",
+		"esri/symbols/PictureFillSymbol", 
+		"esri/symbols/CartographicLineSymbol", 
+		"esri/graphic", 
+		"esri/Color", 
+		"dojo/dom", 
+		"dojo/on",
       "dojo/dom-construct",
       "dojo/domReady!"
       
@@ -38,34 +47,15 @@
 		arcgisUtils,
 		parser,
 		FeatureLayer,
+		 Draw,
+        SimpleMarkerSymbol, SimpleLineSymbol,
+        PictureFillSymbol, CartographicLineSymbol, 
+        Graphic, 
+        Color, dom, on,
 		domConstruct
 
     ) {
       parser.parse();
-
-
-
-
-
-
-
-	// require([
-	//   "esri/map", 
-	//   "esri/dijit/BasemapGallery", 
-	//   "esri/arcgis/utils",
-	//   "dojo/parser",
-	// 	// 	"esri/InfoTemplate",
-	// 	// "esri/layers/FeatureLayer",
-	// 	"dojo/dom-construct",
-	//   "dijit/layout/BorderContainer", 
-	//   "dijit/layout/ContentPane", 
-	//   "dijit/TitlePane",
-	//   "dojo/domReady!"
-	// ], function(
-	//   Map, BasemapGallery, arcgisUtils,  parser
-	  
-	// ) {
-	//   parser.parse();
 
 	  map = new Map("map", {
 		center: [-70.35, 41.68],
@@ -86,23 +76,76 @@
 		console.log("basemap gallery error:  ", msg);
 	  });
 
-
-	  // var template = new InfoTemplate();
-			// template.setTitle("<b>${EMBAY_DISP}</b>");
-			// template.setContent("<a href='{{url('map')}}/${EMBAY_ID}'>Create a scenario for ${EMBAY_DISP}</a>");  
-
-	  var embayLayer = new FeatureLayer("http://gis-services.capecodcommission.org/arcgis/rest/services/wMVP/wMVP3/MapServer/4", {
+	   var embayLayer = new FeatureLayer("http://gis-services.capecodcommission.org/arcgis/rest/services/wMVP/wMVP3/MapServer/4", {
 				mode: FeatureLayer.MODE_ONDEMAND
 				// infoTemplate:template,
 				// outFields: ["EMBAY_DISP", "EMBAY_ID"]
 			});
 			embayLayer.show();
 			map.addLayer(embayLayer);
+
+ var fillSymbol = new PictureFillSymbol(
+          "images/mangrove.png",
+          new SimpleLineSymbol(
+            SimpleLineSymbol.STYLE_SOLID,
+            new Color('#000'), 
+            1
+          ), 
+          42, 
+          42
+        );
+
+  function initToolbar() {
+          tb = new Draw(map);
+          tb.on("draw-end", addGraphic);
+
+          // event delegation so a click handler is not
+          // needed for each individual button
+          on(dom.byId("info"), "click", function(evt) {
+            if ( evt.target.id === "info" ) {
+              return;
+            }
+            var tool = evt.target.id.toLowerCase();
+            map.disableMapNavigation();
+            tb.activate(tool);
+          });
+        }
+
+        function addGraphic(evt) {
+          //deactivate the toolbar and clear existing graphics 
+          tb.deactivate(); 
+          map.enableMapNavigation();
+
+          // figure out which symbol to use
+          var symbol;
+          if ( evt.geometry.type === "point" || evt.geometry.type === "multipoint") {
+            symbol = markerSymbol;
+          } else if ( evt.geometry.type === "line" || evt.geometry.type === "polyline") {
+            symbol = lineSymbol;
+          }
+          else {
+            symbol = fillSymbol;
+          }
+
+          map.graphics.add(new Graphic(evt.geometry, symbol));
+        }
+
+	  // var template = new InfoTemplate();
+			// template.setTitle("<b>${EMBAY_DISP}</b>");
+			// template.setContent("<a href='{{url('map')}}/${EMBAY_ID}'>Create a scenario for ${EMBAY_DISP}</a>");  
+
+	 
 	});
   </script> 
 </head> 
 
 <body class="claro"> 
+	<div id="info">
+      <div>Select a shape then draw on map to add graphic</div>
+
+      <button id="Polygon">Polygon</button>
+
+    </div>
   <div data-dojo-type="dijit/layout/BorderContainer" 
 	   data-dojo-props="design:'headline', gutters:false" 
 	   style="width:100%;height:100%;margin:0;">
@@ -122,6 +165,8 @@
 	  </div>
 
 	</div>
+
+
   </div>
 </body> 
 
