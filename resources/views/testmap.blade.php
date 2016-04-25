58228f0a38a0cffe52ded33e891536eb4d60df02
Sue <smalomo@20miletech.com>
2016-04-25 13:28:54 -0400

    add polygon drawing tool to testmap

--- resources/views/testmap.blade.php
+++ resources/views/testmap.blade.php
@@ -4,58 +4,74 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   
   <meta name="viewport" content="initial-scale=1, maximum-scale=1,user-scalable=no"/>
-  <title></title>
+  <title>Testing Map Features</title>
 
-  <link rel="stylesheet" href="https://js.arcgis.com/3.16/dijit/themes/claro/claro.css">    
-  <link rel="stylesheet" href="https://js.arcgis.com/3.16/esri/css/esri.css">
+  <link rel="stylesheet" href="https://js.arcgis.com/3.16/dijit/themes/claro/claro.css" />    
+  <link rel="stylesheet" href="https://js.arcgis.com/3.16/esri/css/esri.css" />
   <style> 
 	html, body { height: 100%; width: 100%; margin: 0; padding: 0; }
-	#map{
-	  padding:0;
-	}
+	#map {   padding: 0; height:100%; width: 100%;	}
   </style> 
   
-  <script src="https://js.arcgis.com/3.16/"></script>
+  <script src= "https://js.arcgis.com/3.16"></script>
   <script> 
 	var map;
-	
+	var tb;
 
 	require([
-      "esri/map", 
-      "esri/dijit/BasemapGallery", 
-      "esri/arcgis/utils",
-      "dojo/parser",
-      "esri/layers/FeatureLayer",
-      "dijit/layout/BorderContainer", 
-      "dijit/layout/ContentPane", 
-      "dijit/TitlePane",
-      "esri/toolbars/draw",
-		"esri/symbols/SimpleMarkerSymbol", 
-		"esri/symbols/SimpleLineSymbol",
-		"esri/symbols/PictureFillSymbol", 
-		"esri/symbols/CartographicLineSymbol", 
-		"esri/graphic", 
-		"esri/Color", 
-		"dojo/dom", 
-		"dojo/on",
-      "dojo/dom-construct",
-      "dojo/domReady!"
-      
-    ], function(
-		Map, 
-		BasemapGallery, 
-		arcgisUtils,
-		parser,
-		FeatureLayer,
-		 Draw,
+
+    "esri/map",
+    "esri/layers/FeatureLayer",
+     "esri/toolbars/draw",
+        "esri/symbols/SimpleMarkerSymbol", "esri/symbols/SimpleLineSymbol",
+        "esri/symbols/SimpleFillSymbol", "esri/symbols/CartographicLineSymbol", 
+        "esri/graphic", 
+        "esri/Color", "dojo/dom", "dojo/on", "dojo/domReady!"
+      ], function(
+        Map, 
+        FeatureLayer,
+        Draw,
         SimpleMarkerSymbol, SimpleLineSymbol,
-        PictureFillSymbol, CartographicLineSymbol, 
+        SimpleFillSymbol, CartographicLineSymbol, 
         Graphic, 
-        Color, dom, on,
-		domConstruct
+        Color, dom, on
+  //     "esri/map", 
+  //     "esri/dijit/BasemapGallery", 
+  //     "esri/arcgis/utils",
+  //     "dojo/parser",
+  //     "esri/layers/FeatureLayer",
+  //     "dijit/layout/BorderContainer", 
+  //     "dijit/layout/ContentPane", 
+  //     // "dijit/TitlePane",
+  //     "esri/toolbars/draw",
+		// // "esri/symbols/SimpleMarkerSymbol", 
+		// "esri/symbols/SimpleLineSymbol",
+		// "esri/symbols/SimpleFillSymbol", 
+		// // "esri/symbols/CartographicLineSymbol", 
+		// "esri/graphic", 
+		// "esri/Color", 
+		// "dojo/dom", 
+		// "dojo/on",
+  //     "dojo/dom-construct",
+  //     "dojo/domReady!"
+      
+  //   ], function(
+		// Map, 
+		// BasemapGallery, 
+		// arcgisUtils,
+		// parser,
+		// FeatureLayer,
+		//  Draw,
+  //       // SimpleMarkerSymbol, 
+  //       SimpleLineSymbol,
+  //       SimpleFillSymbol, 
+  //       // CartographicLineSymbol, 
+  //       Graphic, 
+  //       Color, dom, on,
+		// domConstruct
 
     ) {
-      parser.parse();
+      // parser.parse();
 
 	  map = new Map("map", {
 		center: [-70.35, 41.68],
@@ -64,17 +80,17 @@
 		slider: true,
 		sliderOrientation: "horizontal"
 	});
-
+   map.on("load", initToolbar);
 	  //add the basemap gallery, in this case we'll display maps from ArcGIS.com including bing maps
-	  var basemapGallery = new BasemapGallery({
-		showArcGISBasemaps: true,
-		map: map
-	  }, "basemapGallery");
-	  basemapGallery.startup();
+	 //  var basemapGallery = new BasemapGallery({
+		// showArcGISBasemaps: true,
+		// map: map
+	 //  }, "basemapGallery");
+	 //  basemapGallery.startup();
 	  
-	  basemapGallery.on("error", function(msg) {
-		console.log("basemap gallery error:  ", msg);
-	  });
+	 //  basemapGallery.on("error", function(msg) {
+		// console.log("basemap gallery error:  ", msg);
+	 //  });
 
 	   var embayLayer = new FeatureLayer("http://gis-services.capecodcommission.org/arcgis/rest/services/wMVP/wMVP3/MapServer/4", {
 				mode: FeatureLayer.MODE_ONDEMAND
@@ -84,16 +100,7 @@
 			embayLayer.show();
 			map.addLayer(embayLayer);
 
- var fillSymbol = new PictureFillSymbol(
-          "images/mangrove.png",
-          new SimpleLineSymbol(
-            SimpleLineSymbol.STYLE_SOLID,
-            new Color('#000'), 
-            1
-          ), 
-          42, 
-          42
-        );
+ var fillSymbol = new SimpleFillSymbol();
 
   function initToolbar() {
           tb = new Draw(map);
@@ -118,14 +125,7 @@
 
           // figure out which symbol to use
           var symbol;
-          if ( evt.geometry.type === "point" || evt.geometry.type === "multipoint") {
-            symbol = markerSymbol;
-          } else if ( evt.geometry.type === "line" || evt.geometry.type === "polyline") {
-            symbol = lineSymbol;
-          }
-          else {
             symbol = fillSymbol;
-          }
 
           map.graphics.add(new Graphic(evt.geometry, symbol));
         }
@@ -150,10 +150,7 @@
 	   data-dojo-props="design:'headline', gutters:false" 
 	   style="width:100%;height:100%;margin:0;">
 
-	<div id="map" 
-		 data-dojo-type="dijit/layout/ContentPane" 
-		 data-dojo-props="region:'center'" 
-		 style="padding:0;">
+	<div id="map">
 
 	  <div style="position:absolute; right:20px; top:10px; z-Index:999;">
 		<div data-dojo-type="dijit/TitlePane" 
