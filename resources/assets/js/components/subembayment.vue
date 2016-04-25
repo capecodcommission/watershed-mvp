1c5649d46a37fcf7a546f2bd8fc964fb33b3aac7
Sue <sue@bluegear.io>
2016-04-25 09:09:11 -0400

    testing map - need basemap gallery and feature layer to work together

---
+++ resources/assets/js/components/subembayment.vue
@@ -0,0 +1,14 @@
+Vue.component('subembayment', {
+		template: '#subembayment-template',
+		props: ['id', 'title', 'percent', 'NLoad_Orig', 'NLoad_Target', 'my-effective'],
+		computed: {
+			NLoad_Current: function()
+				{
+					return this.NLoad_Orig * ((100 - this.myEffective)/100)
+				},
+			percent: function()
+			{
+				return (this.NLoad_Target/this.NLoad_Current * 100)
+			}
+		}
+	});
