60ab00e8a74f47183a491e120ead55862f4bb465
Shawn Goulet <shawn.goulet@gmail.com>
2016-11-28 11:27:27 -0500

    changed 'acquaculture' to 'aquaculture' and added 3 missing technologies (tecnology IDs 2,5 & 37).  These changes all happen in the wizard-steps.blade.php

--- resources/views/common/wizard-steps.blade.php
+++ resources/views/common/wizard-steps.blade.php
@@ -16,7 +16,7 @@
 			</header>
 		</div>
 		<div class="acc_cCont">
-		
+
 		<p><a class="button--cta right" href="{{url('results', session('scenarioid'))}}" target="wmvp_results_{{session('scenarioid')}}">View Scenario Summary</a>
 			<span class="remaining"><strong>Nitrogen Remaining to Threshold:</strong> <span></span>kg</span>
 			</p>
@@ -30,9 +30,9 @@
 				<li>Groundwater</li>
 				<li>Embayment</li>
 			</ul>
-										
+
 			<p>For each of these stages, you can select technologies to remove Nitrogen from the embayment. For some, you can select the area that will be treated by drawing a polygon on the map. Your progress towards the embayment's Target Nitrogen Removal will be displayed in the graph to the left. In addition to the overall target, each sub-embayment will have its own individual Nitrogen load and target, which you can track using the graphs in the left sidebar.</p>
-		
+
 		</div>
 	</article>
 	</aside>
@@ -61,7 +61,7 @@
 			@else
 				<div class="technology">
 					<a href="/tech/fert/25" class="popdown"><img src="http://www.cch2o.org/Matrix/icons/npk_mgt.svg"></a><br />
-						Fertilizer Management		
+						Fertilizer Management
 				</div>
 
 			@endif
@@ -70,7 +70,7 @@
 		</div>
 	</article>
 	</aside>
-	
+
 	<aside class="accHorizontal__item">
 		<input type="radio" name="group-1" class="state" id="acc-2" />
 		<label class="backdrop" for="acc-2"></label>
@@ -86,7 +86,7 @@
 				<span class="remaining"><strong>Nitrogen Remaining to Threshold:</strong> <span></span>kg</span>
 				</p>
 				<hr>
-				
+
 				<div class="technology_list">
 				@if(session('storm_applied')==1)
 					<div class="technology disabled">
@@ -126,28 +126,28 @@
 							<img src="http://www.cch2o.org/Matrix/icons/stormwater_bmps.svg"><br />
 							Vegetated Swale
 						</a>
-					</div>	 
+					</div>
 					<div class="technology">
 						<a href="{{url('/tech/storm/10')}}"  class="popdown">
 							<img src="http://www.cch2o.org/Matrix/icons/stormwater_constructed_wetlands.svg"><br />
 							Constructed Wetlands
 						</a>
-					</div>		
+					</div>
 					<div id="info">
 
 					</div>
 				</div>
-							 
+
 			</div>
 		</article>
 	</aside>
-	
+
 	<aside class="accHorizontal__item">
 	<input type="radio" name="group-1" class="state" id="acc-3" />
 	<label class="backdrop" for="acc-3"><!-- <i class="fa fa-times"></i> --></label>
 	<article class="acc_cBox">
 		<div class="acc_cImg">
-		
+
 		<header>
 			<h3>Septic</h3><img src="http://www.cch2o.org/Matrix/icons/reduction.svg" alt="" width="60" style="display: inline;">
 		</header>
@@ -171,57 +171,62 @@
 				<a href="{{url('/tech/collect/42')}}" class="popdown">
 					<img src="http://www.cch2o.org/Matrix/icons/conventional_treatment.svg" alt="" ><br />
 					Conventional Treatment</a>
-			</div>			
+			</div>
 			<div class="technology">
 				<a href="{{url('/tech/collect/43')}}" class="popdown">
 					<img src="http://www.cch2o.org/Matrix/icons/advanced_treatment.svg" alt="" ><br />
 					Advanced Treatment</a>
-			</div>			
+			</div>
 			<div class="technology">
 				<a href="{{url('/tech/collect/44')}}" class="popdown">
 					<img src="http://www.cch2o.org/Matrix/icons/cluster_3.svg" alt="" ><br />
 					Satellite Treatment</a>
-			</div>	
+			</div>
 			<div class="technology">
 				<a href="{{url('/tech/collect/45')}}" class="popdown">
 					<img src="http://www.cch2o.org/Matrix/icons/cluster_3.svg" alt="" ><br />
 					Satellite Treatment - Enahnced</a>
-			</div>	
+			</div>
 			<div class="technology">
 				<a href="{{url('/tech/septic/21')}}" class="popdown">
 					<img src="http://www.cch2o.org/Matrix/icons/toilet.svg" alt="" ><br />
 					Composting Toilets</a>
-			</div>	
+			</div>
 			<div class="technology">
 				<a href="{{url('/tech/septic/22')}}" class="popdown">
 					<img src="http://www.cch2o.org/Matrix/icons/toilet.svg" alt="" ><br />
 					Incinerating Toilets</a>
-			</div>	
+			</div>
 
 			<div class="technology">
 				<a href="{{url('/tech/septic/23')}}" class="popdown">
 					<img src="http://www.cch2o.org/Matrix/icons/toilet.svg" alt="" ><br />
 					Packaging Toilets</a>
-			</div>		
+			</div>
 			<div class="technology">
 				<a href="{{url('/tech/septic/24')}}" class="popdown">
 					<img src="http://www.cch2o.org/Matrix/icons/toilet.svg" alt="" ><br />
 					Urine Diverting Toilets</a>
-			</div>		
+			</div>
 			<div class="technology">
 				<a href="{{url('/tech/septic/38')}}" class="popdown">
 					<img src="http://www.cch2o.org/Matrix/icons/ia.svg" alt="" ><br />
 					I/A Systems</a>
-			</div>		
+			</div>
 			<div class="technology">
 				<a href="{{url('/tech/septic/39')}}" class="popdown">
 					<img src="http://www.cch2o.org/Matrix/icons/enhanced_ia.svg" alt="" ><br />
 					Enhanced I/A Systems</a>
-			</div>						
+			</div>
+			<div class="technology">
+				<a href="{{url('/tech/septic/37')}}" class="popdown">
+					<img src="http://www.cch2o.org/Matrix/icons/titl3_5.svg" alt="" ><br />
+					Title 5 Septic System Replacement (Base Line Condition)</a>
+			</div>
 		</div>
 	</article>
 	</aside>
-	
+
 	<aside class="accHorizontal__item">
 	<input type="radio" name="group-1" class="state" id="acc-4" />
 	<label class="backdrop" for="acc-4"><!-- <i class="fa fa-times"></i> --></label>
@@ -249,62 +254,74 @@
 						<img src="http://www.cch2o.org/Matrix/icons/constructed_wetland.svg" alt=""><br />
 						Constructed Wetlands - Groundwater Flow
 					</a>
-				</div>	
+				</div>
+				<div class="technology">
+					<a href="{{url('/tech/groundwater/2')}}"  class="popdown">
+						<img src="http://www.cch2o.org/Matrix/icons/constructed_wetland.svg" alt=""><br />
+						Constructed Wetlands - Subsurface Flow
+					</a>
+				</div>
+				<div class="technology">
+					<a href="{{url('/tech/groundwater/5')}}"  class="popdown">
+						<img src="http://www.cch2o.org/Matrix/icons/phytoirrigation.svg" alt=""><br />
+						Phytoirrigation
+					</a>
+				</div>
 				<div class="technology">
 					<a href="{{url('/tech/groundwater/4')}}"  class="popdown">
 						<img src="http://www.cch2o.org/Matrix/icons/hydroponic.svg" alt=""><br />
 						Hydroponic Treatment
 					</a>
-				</div>				
+				</div>
 				<div class="technology">
 					<a href="{{url('/tech/groundwater/14')}}"  class="popdown">
 						<img src="http://www.cch2o.org/Matrix/icons/phytoremediation.svg" alt=""><br />
 						Phytoremediation
 					</a>
-				</div>	
+				</div>
 				<div class="technology">
 					<a href="{{url('/tech/groundwater/15')}}"  class="popdown">
 						<img src="http://www.cch2o.org/Matrix/icons/prb.svg" alt=""><br />
 						PRB - Trench
 					</a>
-				</div>		
+				</div>
 				<div class="technology">
 					<a href="{{url('/tech/groundwater/16')}}"  class="popdown">
 						<img src="http://www.cch2o.org/Matrix/icons/prb.svg" alt=""><br />
 						PRB - Injection Well (30')
 					</a>
-				</div>		
+				</div>
 				<div class="technology">
 					<a href="{{url('/tech/groundwater/17')}}"  class="popdown">
 						<img src="http://www.cch2o.org/Matrix/icons/prb.svg" alt=""><br />
 						PRB - Injection Well (45')
 					</a>
-				</div>	
+				</div>
 				<div class="technology">
 					<a href="{{url('/tech/groundwater/18')}}"  class="popdown">
 						<img src="http://www.cch2o.org/Matrix/icons/prb.svg" alt=""><br />
 						PRB - Injection Well (60')
 					</a>
-				</div>					
+				</div>
 				<div class="technology">
 					<a href="{{url('/tech/groundwater/19')}}"  class="popdown">
 						<img src="http://www.cch2o.org/Matrix/icons/fertigation_wells.svg" alt=""><br />
 						Fertigation Wells (Turf)
 					</a>
-				</div>		
+				</div>
 				<div class="technology">
 					<a href="{{url('/tech/groundwater/20')}}"  class="popdown">
 						<img src="http://www.cch2o.org/Matrix/icons/fertigation_wells.svg" alt=""><br />
 						Fertigation Wells (Cranberry bogs)
 					</a>
-				</div>					
+				</div>
 				<div class="technology">
 					<a href="{{url('/tech/groundwater/34')}}"  class="popdown">
 						<img src="http://www.cch2o.org/Matrix/icons/surface_water_remediation_wetlands.svg" alt=""><br />
 						Surface Water Remediation Wetlands
 					</a>
 				</div>
-			</div>		
+			</div>
 		</div>
 	</article>
 	</aside>
@@ -325,19 +342,19 @@
 			<div class="technology">
 				<a href="{{url('/tech/embayment/11')}}"  class="popdown">
 					<img src="http://www.cch2o.org/Matrix/icons/aquaculture_shellfishing.svg" alt=""><br />
-					Acquaculture in Estuary Bed
+					Aquaculture in Estuary Bed
 				</a>
 			</div>
 			<div class="technology">
 				<a href="{{url('/tech/embayment/12')}}"  class="popdown">
 					<img src="http://www.cch2o.org/Matrix/icons/aquaculture_shellfishing.svg" alt=""><br />
-					Acquaculture Above Estuary Bed
+					Aquaculture Above Estuary Bed
 				</a>
 			</div>
 			<div class="technology">
 				<a href="{{url('/tech/embayment/13')}}"  class="popdown">
 					<img src="http://www.cch2o.org/Matrix/icons/aquaculture_shellfishing.svg" alt=""><br />
-					Acquaculture - Mariculture
+					Aquaculture - Mariculture
 				</a>
 			</div>
 			<div class="technology">
@@ -357,27 +374,27 @@
 					<img src="http://www.cch2o.org/Matrix/icons/constructed_wetlands_floating.svg" alt=""><br />
 					Floating Constructed Wetlands
 				</a>
-			</div>			
+			</div>
 		</div>
 	</article>
 	</aside>
-	
+
 	<aside class="accHorizontal__item">
 	<input type="radio" name="group-1" class="state" id="acc-6" />
 	<label class="backdrop" for="acc-6"><!-- <i class="fa fa-times"></i> --></label>
 	<article class="acc_cBox">
 		<div class="acc_cImg">
-		
+
 		<header>
 			<h3>Summary</h3>
-			
+
 		</header>
 		</div>
 		<div class="acc_cCont">
 		<p><span class="remaining"><strong>Nitrogen Remaining to Threshold:</strong> <span></span>kg</span></p>
 
 		<p>Overall Nitrogen Reduction: </p>
-			
+
 			<p><a href="{{url('download', session('scenarioid'))}}">Download Results (.xls)</a></p>
 			<p><a href="{{url('results', session('scenarioid'))}}" class="button" target="wmvp_results_{{session('scenarioid')}}">View detailed results</a></p>
 		</div>
