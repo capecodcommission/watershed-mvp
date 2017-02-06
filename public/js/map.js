06cda53e238e1a4f96d70d2db25199ea01dd0729
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2017-02-06 14:22:12 -0500

    added number formatting to nitrogen variables

--- public/js/map.js
+++ public/js/map.js
@@ -460,45 +460,15 @@ require([
 
 			title: "Info", 
 			content: "<table class = 'table'><tbody>" +
-						"<tr>" + "<td>" + "Water Use (Gal/Day): " + "</td>" + "<td>" + "${WaterUseExisting}" + "</td>" + "</tr>" +
-						"<tr>" + "<td>" + "Waste Water Treatment (Gal/Day): " + "</td>" + "<td>" + "${WWTreatmentExisting}" + "</td>" + "</tr>" +
+						"<tr>" + "<td>" + "Water Use (Gal/Day): " + "</td>" + "<td>" + "${WaterUseExisting:NumberFormat(places:2)}" + "</td>" + "</tr>" +
+						"<tr>" + "<td>" + "Waste Water Treatment: " + "</td>" + "<td>" + "${WWTreatmentExisting}" + "</td>" + "</tr>" +
 						"<tr>" + "<td>" + "Land Use Category: " + "</td>" + "<td>" + "${LandUseCatExisting}" + "</td>" + "</tr>" +
 						"<tr>" + "<td>" + "Water Use Source: " + "</td>" + "<td>" + "${WaterUseSource}" + "</td>" + "</tr>" +
-						"<tr>" + "<td>" + "Unattn Nitrogen Load (Septic) (Kg/Yr): " + "</td>" + "<td>" + "${NLoad_Septic_Existing}" + "</td>" + "</tr>" +
-						"<tr>" + "<td>" + "Unattn Nitrogen Load (Fertilization) (Kg/Yr): " + "</td>" + "<td>" + "${Nload_Fert}" + "</td>" + "</tr>" +
-						"<tr>" + "<td>" + "Unattn Nitrogen Load (Stormwater) (Kg/Yr): " + "</td>" + "<td>" + "${Nload_Stormwater}" + "</td>" + "</tr>" +
-						"<tr>" + "<td>" + "Unattn Nitrogen Load (Atmosphere) (Kg/Yr): " + "</td>" + "<td>" + "${Nload_Atmosphere}" + "</td>" + "</tr>" +
-						"<tr>" + "<td>" + "Unattn Nitrogen Load (Full) (Kg/Yr): " + "</td>" + "<td>" + "${Nload_Full}" + "</td>" + "</tr>" + "</tbody></table>",
-			fieldInfos: [{
-				fieldName: "WaterUseExisting",
-				format: {
-					places: 2
-				},
-				fieldName: "WWTreatmentExisting",
-				format: {
-					places: 2
-				},
-				fieldName: "NLoad_Septic_Existing",
-				format: {
-					places: 2
-				},
-				fieldName: "Nload_Fert",
-				format: {
-					places: 2
-				},
-				fieldName: "Nload_Stormwater",
-				format: {
-					places: 2
-				},
-				fieldName: "Nload_Atmosphere",
-				format: {
-					places: 2
-				},
-				fieldName: "Nload_Full",
-				format: {
-					places: 2
-				}
-			}]
+						"<tr>" + "<td>" + "Unattn Nitrogen Load (Septic) (Kg/Yr): " + "</td>" + "<td>" + "${NLoad_Septic_Existing:NumberFormat(places:2)}" + "</td>" + "</tr>" +
+						"<tr>" + "<td>" + "Unattn Nitrogen Load (Fertilization) (Kg/Yr): " + "</td>" + "<td>" + "${Nload_Fert:NumberFormat(places:2)}" + "</td>" + "</tr>" +
+						"<tr>" + "<td>" + "Unattn Nitrogen Load (Stormwater) (Kg/Yr): " + "</td>" + "<td>" + "${Nload_Stormwater:NumberFormat(places:2)}" + "</td>" + "</tr>" +
+						"<tr>" + "<td>" + "Unattn Nitrogen Load (Atmosphere) (Kg/Yr): " + "</td>" + "<td>" + "${Nload_Atmosphere:NumberFormat(places:2)}" + "</td>" + "</tr>" +
+						"<tr>" + "<td>" + "Unattn Nitrogen Load (Full) (Kg/Yr): " + "</td>" + "<td>" + "${Nload_Full:NumberFormat(places:2)}" + "</td>" + "</tr>" + "</tbody></table>"
 		});
 
 
