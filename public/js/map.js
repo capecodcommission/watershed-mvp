1899c941c13337139e17c52868e74ea231e6f658
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2017-02-10 11:03:50 -0500

    symbol size

--- public/js/map.js
+++ public/js/map.js
@@ -525,7 +525,7 @@ require([
 		var wasteRenderer = new SimpleRenderer(wasteSymbol)
 			wasteRenderer.setSizeInfo({
 	        	field: "WWFlowsExisting",
-	        	minSize: 1,
+	        	minSize: 10,
 	        	maxSize: 20,
 	        	minDataValue: 0,
 	        	maxDataValue: 10000
