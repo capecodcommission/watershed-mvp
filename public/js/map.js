01a748bf1773c441a2b706b49f54a9231e22c9b1
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2017-02-14 14:45:21 -0500

    m

--- public/js/map.js
+++ public/js/map.js
@@ -111,7 +111,7 @@ require([
 		// map.on("load", initToolbar);
 		map.on("load", function(e){
 			initToolbar();
-			map.infoWindow.resize(300,400)
+			map.infoWindow.resize(350,400)
 			if (treatments.length > 0) 
 			{
 				addTreatmentPolygons(treatments);
