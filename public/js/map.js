940770dadc87f4410a6ae105f88e992ffbafbd14
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2017-02-14 13:56:40 -0500

    removed infowindow on polygon click

--- public/js/map.js
+++ public/js/map.js
@@ -134,21 +134,18 @@ require([
 
 				var tool = evt.target.id.toLowerCase();
 				map.disableMapNavigation();
-				map.infoWindow.hide();
 				tb.activate(tool);
 			});
 			
 			editToolbar = new Edit(map);
 			// $('.edit_poly').on('click', function(e){
 			on(dom.byId('edit_polygon'), 'click', function(e){
-				map.infoWindow.hide();
 				// console.log(e);
 				edit_active = 1;
 				// polyGLs[0].on('click', function(evt){
 					// console.log(this);
 
 				map.graphics.on("click", function(evt) {
-					map.infoWindow.hide();
 					// console.log(edit_active);
 					if (edit_active > 0) {
 					// console.log(this);
@@ -190,7 +187,6 @@ require([
 			//deactivate the toolbar and clear existing graphics 
 			tb.deactivate();
 			map.enableMapNavigation();
-			// map.infoWindow.show();
 			// console.log(treatment);
 			// figure out which symbol to use
 			var symbol;
