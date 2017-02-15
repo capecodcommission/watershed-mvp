68afa53a1ba2cae22370962dbe1b6c12e4c8d33a
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2017-02-15 13:38:26 -0500

    format legend

--- resources/views/common/map-tools.blade.php
+++ resources/views/common/map-tools.blade.php
@@ -16,8 +16,8 @@
     </div>
     <div style="position:absolute; right:250px; top:20px; z-Index:100;">
         <div data-dojo-type="dijit/TitlePane" data-dojo-props="title:'Legend', closable:false, open:false">
-            <div data-dojo-type="dijit/layout/ContentPane" style="width:280px; height:580px; overflow:auto; ">
-                <div style = "height: 100px; top: 0px; right: 0px; padding: 0px 0px 0px 10px; " id="legendDiv"></div>
+            <div data-dojo-type="dijit/layout/ContentPane" style="width:300px; height:580px; overflow:auto; ">
+                <div id="legendDiv"></div>
             </div>
         </div>
     </div>
