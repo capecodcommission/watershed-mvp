b158921b3a263421d232f6209798b305f2272f2c
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2017-02-15 11:08:48 -0500

    format legend

--- public/js/map.js
+++ public/js/map.js
@@ -294,7 +294,7 @@ require([
 								   ),
 							   new Color([0, 0, 0, 0.0])
 							   );
-					var imageURL = "http://www.cch2o.org/Matrix/icons/"+Treatment.treatment_icon;
+					var imageURL = "http://2016.watershedmvp.org/images/SVG/"+Treatment.treatment_icon;
 				}
 				// treatments[i]
 				if (Treatment.Custom_POLY == 1) 
