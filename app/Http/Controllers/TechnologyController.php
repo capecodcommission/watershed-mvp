4a2f427f0602b9bcffe024c5e85ddeadd1d664ad
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2017-02-21 13:37:07 -0500

    embay

--- app/Http/Controllers/TechnologyController.php
+++ app/Http/Controllers/TechnologyController.php
@@ -439,18 +439,7 @@ class TechnologyController extends Controller
 					$n_parcels = 0;
 
 					
-					$parcels = DB::raw("select 
-											wtp_town_id, 
-											count(wtp_town_id) as NumParcels, 
-											sum(wtp_nload_septic) as Septic, 
-											sum(wtp_nload_fert) as Fert,	
-											sum(wtp_nload_storm) as Storm,		
-											sum(wtp_nload_orig) as Original
-
-										from capecodma.wiz_treatment_parcels 
-										where treatment_wiz_id =" + $treat_id + " 
-										group by wtp_town_id"
-									);
+					$parcels = DB::select('exec CapeCodMA.GET_PointsFromPolygon ' . $subemid . ', ' . $scenarioid . ', ' . $treat_id . ', \'subembayment\'');
 					
 
 					foreach ($parcels as $parcel) 
