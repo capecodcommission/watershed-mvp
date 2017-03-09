c6745cee2c8019b29f86ec447bd53a2722cbc77e
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2017-03-09 16:06:15 -0500

    console log treatment in addgraphic

--- public/js/map.js
+++ public/js/map.js
@@ -195,7 +195,7 @@ require([
 			//deactivate the toolbar and clear existing graphics 
 			tb.deactivate();
 			map.enableMapNavigation();
-			console.log(treatment);
+			console.log(evt);
 			// figure out which symbol to use
 			var symbol;
 			symbol = fillSymbol;
