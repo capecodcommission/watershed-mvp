e5636866767034c82d93efa79cd625da951e5123
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2017-03-20 13:59:42 -0400

    modified location.reload

--- resources/views/common/technology-septic.blade.php
+++ resources/views/common/technology-septic.blade.php
@@ -84,10 +84,11 @@
 					$( "#update" ).trigger( "click" );
 					var newtreatment = '<li class="technology" data-treatment="{{$treatment->TreatmentID}}"><a href="{{url('/edit', $treatment->TreatmentID)}}" class="popdown"><img src="http://2016.watershedmvp.org/images/SVG/{{$tech->Icon}}" alt=""></a></li>';
 					$('ul.selected-treatments').append(newtreatment);
-					$('ul.selected-treatments li[data-treatment="{{$treatment->TreatmentID}}"] a').popdown();					
+					$('ul.selected-treatments li[data-treatment="{{$treatment->TreatmentID}}"] a').popdown();	
+					location.reload()				
 				});
 
-			location.reload()
+			// location.reload()
 		});
 
 
