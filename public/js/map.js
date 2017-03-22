31ed161883b238fd0d56fffbb37074862ebe57ea
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2017-03-22 11:49:38 -0400

    added typeid

--- public/js/map.js
+++ public/js/map.js
@@ -198,7 +198,7 @@ require([
 		************************************/
 		function addGraphic(evt) {
 
-			console.log(typeid)
+			console.log(this.typeid)
 
 			//deactivate the toolbar and clear existing graphics 
 			tb.deactivate();
