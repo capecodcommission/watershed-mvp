2fc6a7d89e3fba1a86555bc3f07f619daa92bbad
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2017-03-23 08:58:45 -0400

    replaced nested if statement in collection-edit@deletetreatment

--- resources/views/common/technology-collection-edit.blade.php
+++ resources/views/common/technology-collection-edit.blade.php
@@ -130,9 +130,12 @@
 
 				for (var i = map.graphics.graphics.length - 1; i >= 0; i--) {
                 
-	                if (map.graphics.graphics[i].attributes & map.graphics.graphics[i].attributes.treatment_id == treatment) {
+	                if (map.graphics.graphics[i].attributes) {
 
-	                	map.graphics.remove(map.graphics.graphics[i])
+	                    if (map.graphics.graphics[i].attributes.treatment_id == treatment) {
+
+	                    	map.graphics.remove(map.graphics.graphics[i])
+	                    }
 	                }
            		}
 			});
