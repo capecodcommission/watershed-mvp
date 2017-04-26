785654c7b4ddcc54c7f929b26b6a9eb89abf915f
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2017-04-26 12:46:25 -0400

    reconfigured pointgls

--- public/js/map.js
+++ public/js/map.js
@@ -979,7 +979,7 @@ require([
                                     // + "<button class='save_poly' data-treatment='"+Treatment.TreatmentID+"'>Save Polygon</button></div>"
                         });
 
-                        pointGLs[i].add(pointGraphic.setInfoTemplate(template))  
+                        pointGLs[i].addMany(pointGraphic.setInfoTemplate(template))  
 
                         // console.log(pointGLs[i])
 
