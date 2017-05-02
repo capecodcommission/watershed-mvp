98755af006f366716a0e614ff983881afd039499
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2017-05-02 15:38:23 -0400

    edit PRB slider value to appear below slider

--- resources/views/common/technology-groundwater.blade.php
+++ resources/views/common/technology-groundwater.blade.php
@@ -44,7 +44,7 @@
 				@endif
 			
 
-			<p align = "left">
+			<p align = "center">
 				Enter a valid reduction rate between {{$tech->Nutri_Reduc_N_Low}} and {{$tech->Nutri_Reduc_N_High}} percent.<br>
 				
 				<input type="range" id="ground-percent" min="{{$tech->Nutri_Reduc_N_Low}}" max="{{$tech->Nutri_Reduc_N_High}}" value="{{$tech->Nutri_Reduc_N_Low}}" v-model="ground_percent">@{{ground_percent}}%
