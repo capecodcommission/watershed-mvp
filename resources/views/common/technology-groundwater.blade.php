53c2c664d3de53c10e49b7b6a06a23d26c37127d
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2017-05-03 13:53:02 -0400

    change popdown opacity to hide before cancelling treatment

--- resources/views/common/technology-groundwater.blade.php
+++ resources/views/common/technology-groundwater.blade.php
@@ -102,6 +102,8 @@
 
 		$('#closeWindow').on('click', function (e) {
 
+			$('#popdown-opacity').hide();
+
 			var url = "{{url('cancel', $treatment->TreatmentID)}}";
 
 			$.ajax({
@@ -109,8 +111,6 @@
 				url: url
 			})
 			.done(function(msg){
-
-				$('#popdown-opacity').hide();
 				
 				for (var i = map.graphics.graphics.length - 1; i >= 0; i--) {
                 
