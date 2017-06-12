c64350d1eb243f4de729f161807dcdf4149084b1
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2017-06-12 15:56:38 -0400

    rework open-in-fim button to open in new tab

--- resources/views/common/wizard-steps.blade.php
+++ resources/views/common/wizard-steps.blade.php
@@ -398,7 +398,7 @@
 
 			<p><a href="{{url('download', session('scenarioid'))}}">Download Results (.xls)</a></p>
 			<p><a href="{{url('results', session('scenarioid'))}}" class="button" target="wmvp_results_{{session('scenarioid')}}">View detailed results</a></p>
-			<p><a class = 'button' href = "http://2016.watershedmvp.org/fim/scenario/{{session('scenarioid')}}/treatmentsDetails">Open Scenario in Financial Model</a></p>
+			<p><a id = 'fim' class = 'button'>Open Scenario in Financial Model</a></p>
 			<p><a id = 'saved' class="save button">Save Changes</a></p>
 		</div>
 	</article>
@@ -415,6 +415,13 @@
 
 			$(this).append("<div class = 'fa fa-spinner fa-spin'></div>")
 			
+		})
+
+		$('#fim').on('click', function(e) {
+
+			// if below doesn't work, add to fim button above ----> href = "http://2016.watershedmvp.org/fim/scenario/{{session('scenarioid')}}/treatmentsDetails"
+
+			window.open("http://2016.watershedmvp.org/fim/scenario/" + scenario + "/treatmentsDetails")
 		})
 
 		$('.save').on('click', function(e){
