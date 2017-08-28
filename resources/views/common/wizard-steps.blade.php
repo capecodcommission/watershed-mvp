d1dc5d38d4a013e141e664f84dad503539a31af6
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2017-08-28 10:30:14 -0400

    reconfigured url injection

--- resources/views/common/wizard-steps.blade.php
+++ resources/views/common/wizard-steps.blade.php
@@ -432,7 +432,7 @@
 			// var scenario = scenario;
 			var path = "http://2016.watershedmvp.org/sam/#/home";
 			// var samSite = window.open(path);
-			var samSite = window.open(path + "/?scenarioID=" + scenario);
+			var samSite = window.open(path + "/" + scenario);
 			// resource: https://stackoverflow.com/questions/133925/javascript-post-request-like-a-form-submit
 			// resource2: https://developer.mozilla.org/en-US/docs/Web/API/Storage/LocalStorage
 
