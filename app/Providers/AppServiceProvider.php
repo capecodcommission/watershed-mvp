fe8eab3e2ee48e92063414d2f11a5de51304aed9
Mario Carloni <mario.carloni@capecodcommission.org>
2019-06-24 12:06:08 -0400

    modified service provider boot function to serve all requests via https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,9 +12,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $urk)
     {
-        URL::forceSchema('https');
+        $url->forceSchema('https');
     }
 
     /**
