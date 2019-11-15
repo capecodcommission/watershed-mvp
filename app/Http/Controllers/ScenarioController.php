bcf22669df1eed83b1e8f73cdd19993a8cb5a0cd
Mario Carloni <mario.carloni@capecodcommission.org>
2019-11-15 13:23:27 -0500

    created stored procedure to obtain treatment nitrogen loads and costs by town, modified scenario results page to prepare for receiving the data returned from the new stored proc

--- app/Http/Controllers/ScenarioController.php
+++ app/Http/Controllers/ScenarioController.php
@@ -84,52 +84,14 @@ class ScenarioController extends Controller
 	}
 	
 
-	/**
-	 * Show results page in the browser
-	 *
-	 * @return void
-	 * @author 
-	 **/
+	// Retrieve scenario-wide technology nitrogen loads and costs by town and subembayment
 	public function getScenarioResults($scenarioid)
 	{	
-		// TODO: Can we get/set from global variable? If so, use that, else findorfail()
 		$scenario = Scenario::find($scenarioid);
-		
-		// $towns = DB::table('CapeCodMA.parcelMaster')
-		// 	->join('CapeCodMA.MAtowns','CapeCodMA.MAtowns.TOWN_ID', '=', 'CapeCodMA.parcelMaster.town_id')
-		// 	->select(
-		// 		DB::raw('CapeCodMA.MATowns.TOWN as town'), 
-		// 		DB::raw('CapeCodMA.parcelMaster.treatment_id as wtt_treatment_id'),
-		// 		DB::raw('count(CapeCodMA.parcelMaster.parcel_id) as wtt_tot_parcels'),
-		// 		DB::raw('sum(CapeCodMA.parcelMaster.running_nload_removed) as wtt_unatt_n_removed')
-		// 	)
-		// 	->where('CapeCodMA.parcelMaster.scenario_id', '=', $scenarioid)
-		// 	->groupBy('CapeCodMA.MAtowns.TOWN','CapeCodMA.parcelMaster.treatment_id')
-		// 	->get();
-
-		// TODO: Can $towns be set and gotten globally? If so, use that, else query
-		$towns = DB::select('
-			select 
-				wtt.*, 
-				t.town 
-
-			from dbo.wiz_treatment_towns wtt 
-			
-			inner join dbo.MATowns t 
-			on t.town_id = wtt.wtt_town_id 
-
-			where wtt.wtt_unatt_n_removed is not null and wtt.wtt_scenario_id = ' . $scenarioid);
-
-		// $subembayments = DB::select('exec CapeCodMA.Calc_ScenarioNitrogen_Subembayments ' . $scenarioid);
-
-		// TODO: Check if we can remove stored proc and get $subembayments global variable to pass into view
+		$towns =  DB::select('exec dbo.GETtreatmentCostsByTown ' . $scenarioid);
 		$subembayments = DB::select('exec dbo.CALCscenarioNitrogenSubembayments ' . $scenarioid);
 
-		//  'subembayments' => session('subembayments')
 		return view('layouts/results', ['scenario'=>$scenario, 'towns'=>$towns, 'subembayments'=>$subembayments]);
-		
-		
-
 	}
 
 	/**
--- resources/views/layouts/results.blade.php
+++ resources/views/layouts/results.blade.php
@@ -11,143 +11,173 @@
 	</head>
 	<body class="results-page">
 		<div class="wrapper">
-			<div class="content full-width">
-				@include('common.navigation')
+		<div align='center' class="content full-width">
+			@include('common.navigation')
 
-				<h1>Scenario ID: {{$scenario->ScenarioID}}</h1>
-	      		<h1>Embayment: {{$scenario->AreaName}}</h1>
-				<h2 class="author">Created by: {{$scenario->user->name}} on {{date('Y-m-d', strtotime($scenario->CreateDate))}}</h2>
-					<div id="app">
-						<?php
-							// TODO: Can we get/set from global variables?
-							$scenario_cost = 0;
-							$n_removed = 0;
-							$n_att_total = 0;
-							$n_att_rem_total = 0;
-							$n_scen_total = 0;
-							$n_target_total = 0;
-							$n_rem_total = 0;
-							setlocale(LC_MONETARY, 'en_US');
-						?>
+			<h1>Scenario ID: {{$scenario->ScenarioID}}</h1>
+	      	<h1>Embayment: {{$scenario->AreaName}}</h1>
+			<h2 class="author">Created by: {{$scenario->user->name}} on {{date('Y-m-d', strtotime($scenario->CreateDate))}}</h2>
+			<div id="app">
+			<?php
+				// TODO: Can we get/set from global variables?
+				$scenario_cost = 0;
+				$scenario_cost_town = 0;
+				$n_removed = 0;
+				$n_removed_town = 0;
+				$n_att_total = 0;
+				$n_att_rem_total = 0;
+				$n_scen_total = 0;
+				$n_target_total = 0;
+				$n_rem_total = 0;
+				setlocale(LC_MONETARY, 'en_US');
+			?>
 
-						@if(count($scenario->treatments) > 0)
+
+			@if(count($scenario->treatments) > 0)
 
 
-						<table>
-							<thead>
-								<tr>
-									<th colspan="2">Technology</th>
-									<th>Parcels Affected</th>
-									<th>Nitrogen Removed (Unatt)</th>
-									<th>Total Cost</th>
-									<th>Cost per kg N removed</th>
-								</tr>
-							</thead>
-						
-							<tbody>
-								@foreach($scenario->treatments as $result)
-								<tr id="treat_{{$result->TreatmentID}}">
-									@if(!$result->Parent_TreatmentId)
-									<td>
-										<div class="technology">
-											<img src="{{$_ENV['CCC_ICONS_SVG'].$result->technology->icon}}" alt="">
-										</div>
-									</td>
-									<td>{{$result->technology->Technology_Strategy}} ({{$result->TreatmentID}})</td>
-									<td>{{$result->Treatment_Parcels}}</td>
-									<td>{{number_format(round($result->Nload_Reduction))}}kg</td> <?php $n_removed += $result->Nload_Reduction; ?>
-									<td><?php echo '$'.number_format($result->Cost_Total,0,'.',',');?></td>
-										<?php $scenario_cost += $result->Cost_Total; ?>
-									<td><?php if ($result->Nload_Reduction > 0) {
-										echo '$'.number_format(($result->Cost_Total/$result->Nload_Reduction)/12.46,0,'.',',');} ?>
-									</td>
-									@endif
-								</tr>
-								@endforeach
-								<tr id="totals">
-									<td>Scenario Totals:</td>
-									<td></td>
-									<td></td>
-									<td><strong><?php echo number_format(round($n_removed));?>kg</strong></td>
-									<td><strong><?php echo '$'.number_format($scenario_cost,0,'.',',');?></strong></td>
-									<td colspan="2"><strong><?php if ($result->Nload_Reduction > 0) {
-										echo '$'.number_format(($scenario_cost/$n_removed)/12.46,0,'.',',');}?></strong>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-						
-						<h2>Towns Affected</h2>
-							<table>
+			<h2>Applied Treatments</h2>
+			<table class = 'treatmentsTable'>
+				<thead>
+					<tr>
+						<th>Technology</th>
+						<th>Towns</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach($scenario->treatments as $treatment)
+						@if(!$treatment->Parent_TreatmentId)
+							<tr>
+								<td style = 'padding-bottom: 1%'>
+										<img src="{{$_ENV['CCC_ICONS_SVG'].$treatment->treatment_icon}}" alt="">
+										<p>{{$treatment->TreatmentType_Name}} ({{$treatment->TreatmentID}}) </p>
+								</td>
+								<td>
+									<table id = 'townsTable'>
+										<thead>
+											<tr>
+												<th>Town</th>
+												<th>Parcels Affected</th>
+												<th>Nitrogen Removed (Unatt)</th>
+												<th>Total Cost</th>
+												<th>Cost per kg N removed</th>
+											</tr>
+										</thead>
+										<tbody>
+											@foreach($towns as $town)
+												@if($town->TreatmentID == $treatment->TreatmentID || $town->Parent_TreatmentId == $treatment->TreatmentID)
+													<tr>
+														<td>{{$town->TOWN}}</td>
+														<td>{{$town->wtt_tot_parcels}}</td>
+														<td><?php echo number_format(round($town->wtt_unatt_n_removed)); $n_removed += $town->wtt_unatt_n_removed; $n_removed_town += $town->wtt_unatt_n_removed; ?>kg</td>
+														<td><?php echo '$'.number_format($town->townCost,0,'.',','); $scenario_cost += $town->townCost; $scenario_cost_town += $town->townCost ?></td>
+														<td><?php echo '$'.number_format($town->costPerKg,0,'.',','); ?></td>
+													</tr>
+												@endif
+											@endforeach
+											<tr id="totals">
+												<td><strong>Totals:</strong></td>
+												<td></td>
+												<td>
+													<strong><?php echo number_format(round($n_removed_town));?>kg</strong>
+												</td>
+												<td>
+													<strong><?php echo '$'.number_format($scenario_cost_town,0,'.',',');?></strong>
+												</td>
+												<td colspan="2">
+													<strong><?php echo '$'.number_format(($scenario_cost_town/$n_removed_town),0,'.',','); ?></strong>
+												</td>
+												<?php $n_removed_town = 0; $scenario_cost_town = 0 ?>
+											</tr>
+										</tbody>
+									</table>
+								</td>
+							</tr>
+						@endif
+					@endforeach
+					<tr style = 'padding-top: 2%' id = 'totals'>
+						<td><strong>Scenario Totals:</strong></td>
+						<td>
+							<table id = 'scenarioTotalsTable'>
 								<thead>
 									<tr>
-										<th>Town</th>
-										<th>Treatment</th>
-										<th>Parcels Affected</th>
-										<th>Nitrogen Removed (unattenuated)</th>
+										<th>Nitrogen Removed (Unatt)</th>
+										<th>Total Cost</th>
+										<th>Cost per kg N removed</th>
 									</tr>
 								</thead>
 								<tbody>
-									@foreach($towns as $town)
 									<tr>
-										<td>{{$town->town}}</td>
-										<td>{{$town->wtt_treatment_id}}</td>
-										<td>{{$town->wtt_tot_parcels}}</td>
-										<td>{{number_format(round($town->wtt_unatt_n_removed))}}kg</td>
-									</tr>
-									@endforeach
-								</tbody>
-							</table>
-						
-						<h2>Subembayments</h2>
-							<table>
-								<thead>
-									<tr>
-										<th>Subembayment</th>
-										<th>Original N<sup>1</sup></th>
-										<th>N Removed (Attenuated)<sup>2</sup></th>
-										<th>Scenario N</th>
-										<th>Threshold N</th>
-										<th>N Remaining to Threshold <sup>3</sup></th>
-									</tr>
-								</thead>
-								<tbody>
-									@foreach($subembayments as $sub)
-									<tr>
-										<td>{{$sub->SUBEM_DISP}}</td>
-										<td>{{number_format(round($sub->n_load_att))}}kg</td> <?php $n_att_total += $sub->n_load_att; ?>
-										<td>{{number_format(round($sub->n_load_att_removed))}}kg</td> <?php $n_att_rem_total += $sub->n_load_att_removed; ?>
-										<td>{{number_format(round($sub->n_load_scenario))}}kg</td> <?php $n_scen_total += $sub->n_load_scenario; ?>
-										<td>{{number_format(round($sub->n_load_target))}}kg</td> <?php $n_target_total += $sub->n_load_target; ?>
-										<td>{{number_format(round($sub->n_load_scenario - $sub->n_load_target))}}kg</td> <?php $n_rem_total += $sub->n_load_scenario - $sub->n_load_target; ?>
-									</tr>
-									@endforeach
-									<tr>
-										<td><strong>Subembayment Totals</strong></td>
-										<td><strong><?php echo number_format(round($n_att_total));?>kg</strong></td>
-										<td><strong><?php echo number_format(round($n_att_rem_total));?>kg</strong></td>
-										<td><strong><?php echo number_format(round($n_scen_total));?>kg</strong></td>
-										<td><strong><?php echo number_format(round($n_target_total));?>kg</strong></td>
-										<td><strong><?php echo number_format(round($n_rem_total));?>kg</strong></td>
+										<td>
+											<strong><?php echo number_format(round($n_removed));?>kg</strong>
+										</td>
+										<td>
+											<strong><?php echo '$'.number_format($scenario_cost,0,'.',',');?></strong>
+										</td>
+										<td colspan="2">
+											<strong><?php echo '$'.number_format(($scenario_cost/$n_removed),0,'.',','); ?></strong>
+										</td>
 									</tr>
 								</tbody>
 							</table>
-			
-							<p><sup>1</sup> The "Original N" value is calculated (attenuated) total Nitrogen for the subembayment. </p>
-							<p><sup>2</sup>A negative number in this column represents Nitrogen added to a subembayment as part of a collection treatment.</p>
-							<p><sup>3</sup>A negative number in this column means the user has exceeded the threshold for this subembayment.</p>
-							<p>
-								<a href="{{url('map', [$scenario->AreaID, $scenario->ScenarioID])}}" class="button" target="wmvp_scenario_{{$scenario->ScenarioID}}">back to map</a> 
-								<a href="{{url('download', $scenario->ScenarioID)}}" class="button--cta right" target="_blank"><i class="fa fa-download"></i> Download Results (.xls)</a>
-								<a id = 'saved' class="save button">Save Changes</a> 
-							</p>
-							@else
-							<p>No treatments have been applied to this scenario yet.</p>
-							<p><a href="{{url('map', [$scenario->AreaID, $scenario->ScenarioID])}}" class="button" target="wmvp_scenario_{{$scenario->ScenarioID}}">Return to map</a> </p>
-							@endif
-					</div>
-				</div>
-			</div>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<h2>Subembayments</h2>
+			<table class = 'treatmentsTable'>
+				<thead>
+					<tr>
+						<th>Subembayment</th>
+						<th>Original N<sup>1</sup></th>
+						<th>N Removed (Attenuated)<sup>2</sup></th>
+						<th>Scenario N</th>
+						<th>Threshold N</th>
+						<th>N Remaining to Threshold <sup>3</sup></th>
+					</tr>
+				</thead>
+				<tbody>
+
+					@foreach($subembayments as $sub)
+					<tr>
+						<td>{{$sub->SUBEM_DISP}}</td>
+						<td>{{number_format(round($sub->n_load_att))}}kg</td> <?php $n_att_total += $sub->n_load_att; ?>
+						<td>{{number_format(round($sub->n_load_att_removed))}}kg</td> <?php $n_att_rem_total += $sub->n_load_att_removed; ?>
+						<td>{{number_format(round($sub->n_load_scenario))}}kg</td> <?php $n_scen_total += $sub->n_load_scenario; ?>
+						<td>{{number_format(round($sub->n_load_target))}}kg</td> <?php $n_target_total += $sub->n_load_target; ?>
+						<td>{{number_format(round($sub->n_load_scenario - $sub->n_load_target))}}kg</td> <?php $n_rem_total += $sub->n_load_scenario - $sub->n_load_target; ?>
+					</tr>
+					@endforeach
+
+					<tr>
+						<td><strong>Subembayment Totals</strong></td>
+						<td><strong><?php echo number_format(round($n_att_total));?>kg</strong></td>
+						<td><strong><?php echo number_format(round($n_att_rem_total));?>kg</strong></td>
+						<td><strong><?php echo number_format(round($n_scen_total));?>kg</strong></td>
+						<td><strong><?php echo number_format(round($n_target_total));?>kg</strong></td>
+						<td><strong><?php echo number_format(round($n_rem_total));?>kg</strong></td>
+					</tr>
+				</tbody>
+
+			</table>
+			<p><sup>1</sup> The "Original N" value is calculated (attenuated) total Nitrogen for the subembayment. </p>
+			<p><sup>2</sup>A negative number in this column represents Nitrogen added to a subembayment as part of a collection treatment.</p>
+			<p><sup>3</sup>A negative number in this column means the user has exceeded the threshold for this subembayment.</p>
+
+			<p>
+				<a href="{{url('map', [$scenario->AreaID, $scenario->ScenarioID])}}" class="button" target="wmvp_scenario_{{$scenario->ScenarioID}}">back to map</a> 
+				<a href="{{url('download', $scenario->ScenarioID)}}" class="button--cta right" target="_blank"><i class="fa fa-download"></i> Download Results (.xls)</a>
+				<a id = 'saved' class="save button">Save Changes</a> 
+			</p>
+
+		@else
+		<p>No treatments have been applied to this scenario yet.</p>
+		<p><a href="{{url('map', [$scenario->AreaID, $scenario->ScenarioID])}}" class="button" target="wmvp_scenario_{{$scenario->ScenarioID}}">Return to map</a> </p>
+		@endif
+
+		</div>
+		</div>
+	</div>
 
 	<script>
 	$(document).ready(function() {
